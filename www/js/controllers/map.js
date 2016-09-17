d7bd3b5e54cd98a8270a2c9dc99311de7514efd1
Seao <wolzaurelien@gmail.com>
2016-09-17 18:59:59 +0300

    [CTRL + VIEW] Adding Google Map

--- www/js/controllers/map.js
+++ www/js/controllers/map.js
@@ -2,6 +2,35 @@
  * [CONTROLLER] MAP
  *********************************/
 
-app.controller('MapCtrl', function($scope, $stateParams) {
-  
+app.controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+  var options = {timeout: 10000, enableHighAccuracy: true};
+  $scope.located = false;
+ 
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
+ 
+    // Position coordinates
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+    // Define options
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    // Create the map
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    $scope.located = true;
+
+    // Adding marker current position
+    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+      var marker = new google.maps.Marker({
+        map: $scope.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
+      });
+    });
+  }, function(error){
+    alert("Could not get location");
+  });
 });
